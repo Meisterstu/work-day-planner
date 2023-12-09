ee74551450f3ebe0b1c593761e3e525b544df404
Meisterstu <s_rainforth@icloud.com>
2023-12-09 18:25:12 +0000

    added todayDate to js

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,8 @@
-dayjs.extend(window.dayjs_plugin_customParseFormat);
+dayjs.extend(window.dayjs_plugin_advancedFormat);
 
 // need today date in header, picking up id currentDay, use dayjs
+const todayDate = dayjs();
+$("#currentDay").text(todayDate.format("dddd, MMMM Do"));
 
 // textAreas need id and class added in html? need to add if else if statements to dynamically add class of past present or future. if adding class in js can leave blank in html, don't need id's? and use single if else statement for all textAreas based on element?
 
